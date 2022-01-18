8a8a7c67caabd01623cf32b33670a6799da6258e
vytcka4 <vytautas.freelance@gmail.com>
2022-01-18 21:09:27 +0200

    commit

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-test;
+// test;
+// Coding Challenge #1
+// Your tasks:
+// 1. Use a constructor function to implement a 'Car'. A car has a 'make' and a
+// 'speed' property. The 'speed' property is the current speed of the car in
+// km/h
+// 2. Implement an 'accelerate' method that will increase the car's speed by 10,
+// and log the new speed to the console
+// 3. Implement a 'brake' method that will decrease the car's speed by 5, and log
+// the new speed to the console
+// 4. Create 2 'Car' objects and experiment with calling 'accelerate' and
+// 'brake' multip
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+Car.prototype.acce = function () {
+  this.speed = this.speed + 10;
+  console.log(this.speed);
+};
+
+Car.prototype.brake = function () {
+  this.speed = this.speed - 5;
+  console.log(this.speed);
+};
+
+const car1 = new Car("audi", 70);
+const car2 = new Car("mersas", 50);
+
+console.log(car1);
+
+car1.acce();
