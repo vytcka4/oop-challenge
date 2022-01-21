4d7ced937190e7d6bcac05264d6e109b069432ba
vytcka4 <vytautas.freelance@gmail.com>
2022-01-21 14:53:26 +0200

    commit

--- script.js
+++ script.js
@@ -101,3 +101,15 @@ tesla.accelarate();
 // Test data:
 // § Data car 1: 'Tesla' going at 120 km/h, with a charge of 23%
 // GOOD LUCK
+
+// Coding Challenge #4
+// Your tasks:
+// 1. Re-create Challenge #3, but this time using ES6 classes: create an 'EVCl'
+// child class of the 'CarCl' class
+// 2. Make the 'charge' property private
+// 3. Implement the ability to chain the 'accelerate' and 'chargeBattery'
+// methods of this class, and also update the 'brake' method in the 'CarCl'
+// class. Then experiment with chaining!
+// Test data:
+// § Data car 1: 'Rivian' going at 120 km/h, with a charge of 23%
+// GOOD LUCK �
