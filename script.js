e08a6b1017c7c4abc5ceaecef0d910b93b58aebf
vytcka4 <vytautas.freelance@gmail.com>
2022-01-21 13:58:48 +0200

    commit

--- script.js
+++ script.js
@@ -54,33 +54,50 @@ class CarCl {
     console.log(this.speed);
   }
   brake() {
-    this.speed = this.speed -5;
+    this.speed = this.speed - 5;
     console.log(this.speed);
   }
 
   get speedUS() {
-    return (this.speed *= 1.6) 
+    return (this.speed *= 1.6);
   }
-  set speedUS(){
+  // set speedUS(){
 
-  }
+  // }
 }
 
+const EV = function (make, speed, charge) {
+  this.charge = charge;
+  Car.call(this, make, speed);
+};
 
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.battery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+EV.prototype.accelarate = function () {
+  this.speed += 20;
+  this.charge -= 1;
+  console.log(
+    `${this.make} going at ${this.speed} and baterry is ${this.charge}`
+  );
+};
+
+const tesla = new EV("tesla", 120, 65);
+tesla.accelarate();
 
 // Your tasks:
 // 1. Use a constructor function to implement an Electric Car (called 'EV') as a child
-// "class" of 'Car'. Besides a make and current speed, the 'EV' also has the 
+// "class" of 'Car'. Besides a make and current speed, the 'EV' also has the
 // current battery charge in % ('charge' property)
-// 2. Implement a 'chargeBattery' method which takes an argument 
+// 2. Implement a 'chargeBattery' method which takes an argument
 // 'chargeTo' and sets the battery charge to 'chargeTo'
-// 3. Implement an 'accelerate' method that will increase the car's speed by 20, 
-// and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 
+// 3. Implement an 'accelerate' method that will increase the car's speed by 20,
+// and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140
 // km/h, with a charge of 22%'
-// 4. Create an electric car object and experiment with calling 'accelerate', 
-// 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when 
+// 4. Create an electric car object and experiment with calling 'accelerate',
+// 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when
 // you 'accelerate'! Hint: Review the definiton of polymorphism �
 // Test data:
 // § Data car 1: 'Tesla' going at 120 km/h, with a charge of 23%
-// GOOD LUCK 
-
+// GOOD LUCK
